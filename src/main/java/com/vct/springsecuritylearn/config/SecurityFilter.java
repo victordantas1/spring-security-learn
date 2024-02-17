d726436c60c7c22197706e479c0c3cceae695399
Victor Dantas <victorr.dantas@gmail.com>
2024-02-17 17:58:12 -0400

    Restrict animal post

--- src/main/java/com/vct/springsecuritylearn/config/SecurityFilter.java
+++ src/main/java/com/vct/springsecuritylearn/config/SecurityFilter.java
@@ -2,6 +2,7 @@ package com.vct.springsecuritylearn.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +20,9 @@ public class SecurityFilter {
         return security.
                 csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
-                        authorize.anyRequest().authenticated())
+                        authorize
+                                .requestMatchers(HttpMethod.POST, "/animais").hasRole("ADMIN")
+                                .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
